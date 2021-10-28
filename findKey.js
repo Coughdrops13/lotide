22c01929b4009ea183b32c8ad9b84920bbc06d3f
Connor Roberts <connoobets@gmail.com>
2021-10-28 16:52:13 -0600

    created a function that finds a key based on a callback function returning true

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎾🎾🎾 Assertion Passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`🏀🏀🏀 Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+
+const findKey = function(object, callback) {
+  const keyArray = Object.keys(object);
+  let result = false;
+  for (const key of keyArray) {
+    if (callback(object[key])) {
+      console.log(key);
+      return result;
+    } 
+  }
+  return result;
+};
+
+findKey({
+  "Blue Hill": { stars: 1, rats: 1000 },
+  "Akaleri":   { stars: 3, rats: 3 },
+  "noma":      { stars: 2, rats: 1000 },
+  "elBulli":   { stars: 3, rats: 1000 },
+  "Ora":       { stars: 2, rats: 1000 },
+  "Akelarre":  { stars: 3, rats: 1000 }
+}, x => x.rats === 3);
